5e39ff22176c9ee07496549348cdcb903fa27b2f
ManChat1 <82063932+ManChat1@users.noreply.github.com>
2025-03-24 18:03:28 -0500

    Update create_test_files.cpp

--- Project1/testing/create_files_testing/create_test_files.cpp
+++ Project1/testing/create_files_testing/create_test_files.cpp
@@ -5,63 +5,62 @@
 #include <random>
 #include <chrono>
 #include <iomanip>
+#include <numeric>
 #include <algorithm>
+
+using namespace std;
 
 // Created by Manan Chaturvedi
 // March 24th, 2025
 // Purpose is to create random CSV files to test for.
 
 int main() {
-    std::vector<std::string> names1 = {"Chuck Lancaster", "Mark Suckerberg", "Andrew Hero", "Micheal Ashton", "Joe Cool", "Jimmy Donaldson", "John Kennedy", "Patrick Star", "Robot Iam", "Alice Wonder"};
-    std::vector<std::string> names2(names1.begin(), names1.begin() + 5);
-    std::vector<std::vector<std::string>> names_list(4, names2);
+    vector<string> names1 = {"Chuck Lancaster", "Mark Suckerberg", "Andrew Hero", "Micheal Ashton", "Joe Cool", "Jimmy Donaldson", "John Kennedy", "Patrick Star", "Robot Iam", "Alice Wonder"};
+    vector<string> names2(names1.begin(), names1.begin() + 5);
+    vector<vector<string>> names_list(4, names2);
     names_list.insert(names_list.end(), 4, names1);
-    std::vector<int> ballots(4, 100);
+    vector<int> ballots(4, 100);
     ballots.insert(ballots.end(), 4, 100000);
-    std::vector<std::string> type = {"S", "P", "S", "P", "S", "P", "S", "P"};
-    std::vector<std::string> path;
+    vector<string> type = {"S", "P", "S", "P", "S", "P", "S", "P"};
+    vector<string> path;
     for (int n = 1; n <= names_list.size(); n++) {
-        path.push_back("test_file" + std::to_string(n) + ".csv");
+        path.push_back("test_file" + to_string(n) + ".csv");
     }
 
-    auto t0 = std::chrono::high_resolution_clock::now();
-    std::random_device generator;
+    auto t0 = chrono::high_resolution_clock::now();
+    random_device generator;
+    mt19937 rd(generator());
     for (int i = 0; i < path.size(); i++) {
-        std::ofstream f(path[i]);
-        std::vector<int> length(names_list[i].size());
-        for (int j = 0; j < length.size(); j++) {
-            length[j] = j + 1;
+        ofstream f(path[i]);
+        vector<int> length(names_list[i].size());
+        iota(length.begin(), length.end(), 1);
+        string str = "";
+        for (string name : names_list[i]) {
+            str += name + ",";
         }
-        std::string string = "";
-        for (auto name : names_list[i]) {
-            string += name + ",";
-        }
-        string.pop_back();
-        f << string << "\n";
+        str.pop_back();
+        f << str << "\n";
+        str = "";
         if (type[i] == "S") {
-            std::vector<std::vector<int>> vals(ballots[i], std::vector<int>(length.size()));
-            for (int b = 0; b < ballots[i]; ++b) {
-                std::shuffle(length.begin(), length.end(), generator);
-                for (int k = 0; k < length.size(); k++) {
-                    vals[b][k] = length[k];
-                }
-            }
-            for (auto row : vals) {
-                for (size_t k = 0; k < row.size(); ++k) {
-                    f << row[k];
-                    if (k < row.size() - 1) f << ",";
-                }
-                f << "\n";
-            }
-        } else if (type[i] == "P") {
-            std::vector<std::vector<int>> vals(ballots[i], std::vector<int>(length.size(), 0));
-            std::uniform_int_distribution<int> distribution(0, length.size() - 1);
+            vector<vector<int>> vals(ballots[i], vector<int>(length.size()));
             for (int b = 0; b < ballots[i]; b++) {
-                int random_index = distribution(generator);
+                shuffle(length.begin(), length.end(), rd);
+                for (int k = 0; k < length.size(); k++) {
+                    str += to_string(length[k]) + ",";
+                }
+                str.pop_back();
+                f << str;
+            }
+            f << "\n";
+        } else if (type[i] == "P") {
+            vector<vector<int>> vals(ballots[i], vector<int>(length.size(), 0));
+            uniform_int_distribution<int> distribution(0, length.size() - 1);
+            for (int b = 0; b < ballots[i]; b++) {
+                int random_index = distribution(rd);
                 vals[b][random_index] = 1;
             }
-            for (auto row : vals) {
-                for (size_t k = 0; k < row.size(); ++k) {
+            for (vector<int> row : vals) {
+                for (int k = 0; k < row.size(); k++) {
                     f << row[k];
                     if (k < row.size() - 1) f << ",";
                 }
@@ -70,9 +69,9 @@ int main() {
         }
         f.close();
     }
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = t1 - t0;
-    std::cout << std::fixed << std::setprecision(2) << diff.count() << " seconds to generate " << path.size() << " test files." << std::endl;
+    auto t1 = chrono::high_resolution_clock::now();
+    chrono::duration<double> diff = t1 - t0;
+    cout << fixed << setprecision(2) << diff.count() << " seconds to generate " << path.size() << " test files." << endl;
 
     return 0;
 }
